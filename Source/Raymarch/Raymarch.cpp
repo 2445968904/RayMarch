cf780904a34d6ad3562ab2e9ad717f09c5914553
2445968904 <2445968904@qq.com>
2023-08-02 10:13:44 +0800

    1

---
+++ Source/Raymarch/Raymarch.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Raymarch.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Raymarch, "Raymarch" );
+
+
+
+
+//IMPLEMENT_PRIMARY_GAME_MODULE(FTestProject,TestProject,"TestProject");
